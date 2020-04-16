133d827767ba0da1e89d2e2e07bd0e37e92db44d
rfargetton <romaric.fargetton@gmail.com>
2020-04-16 23:22:33 +0200

    added gatsby-source-filesystem config

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,13 @@
 module.exports = {
   plugins: [
     `gatsby-plugin-netlify-cms`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/blog`
+      }
+    },
     `gatsby-transformer-remark`
   ]
 };
