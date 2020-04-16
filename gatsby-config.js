6c273abe4ad55c8faf4a0ce7bbc4e306b553db98
rfargetton <romaric.fargetton@gmail.com>
2020-04-16 23:20:13 +0200

    deleted gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,13 +7,6 @@
 module.exports = {
   plugins: [
     `gatsby-plugin-netlify-cms`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `markdown-pages`,
-        path: `${__dirname}/blog`
-      }
-    },
     `gatsby-transformer-remark`
   ]
 };
