d378a1130f7281bab3762ee81011c1cab532ed13
Romaric Fargetton <romaric.fargetton@gmail.com>
2020-09-16 16:08:38 +0200

    tried a fix on meta tags not picked up by social sites

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -10,3 +10,16 @@ export const wrapRootElement = ({ element }) => {
     </ThemeManager>
   )
 }
+
+export const onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
+  const headComponents = getHeadComponents();
+  headComponents.sort((x, y) => {
+    if (x.props && x.props["data-react-helmet"]) {
+      return -1;
+    } else if (y.props && y.props["data-react-helmet"]) {
+      return 1;
+    }
+    return 0;
+  })
+  replaceHeadComponents(headComponents)
+}
